2d467f25d33a8cb59b904a444cbacb9f6d29fdb1
everzhong <everzhong@163.com>
2021-06-03 07:32:02 +0800

    first

---
+++ src/views/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="sys-container home">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index",
+  data() {
+    return {
+      // 版本号
+      version: "1.0.0",
+    };
+  },
+  methods: {
+    goTarget(href) {
+      window.open(href, "_blank");
+    },
+  },
+};
+</script>
+
